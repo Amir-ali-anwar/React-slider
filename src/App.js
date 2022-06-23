573889127ccf865f9d8f06b9630bf78fc65f4cf7
DESKTOP-MTDGO5G\AMIR <amiralianwar611@gmail.com>
2022-06-23 20:17:43 +0500

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [slide,Setslide]=useState(data)
+  console.log(slide);
+  return (
+    <section className='section'>
+      <div className="title"><h2><span>/</span>reviews</h2></div>
+      <div className='section-center'>
+       {slide?.map((item)=>(
+         <article>
+         <img src={item.image} alt="" />
+         <h4>{item.name}</h4>
+         <p className='title'>{item.title}</p>
+         <p className='text'>{item.quote}</p>
+         </article>
+         )) }
+      </div>
+    </section>
+  )
+}
+
+export default App;
